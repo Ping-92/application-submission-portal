c3020597795f6db611545d0be209e41e84e5a322
Ping-92 <60598616+Ping-92@users.noreply.github.com>
2022-10-29 21:29:56 +0800

    updated application service with web form. FTP to be implemented

--- application-service/src/main/java/com/cognixia/controller/ApplicationController.java
+++ application-service/src/main/java/com/cognixia/controller/ApplicationController.java
@@ -1,14 +1,19 @@
 package com.cognixia.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cognixia.model.Application;
 import com.cognixia.service.ApplicationService;
@@ -19,13 +24,26 @@ public class ApplicationController {
 	@Autowired
 	ApplicationService applicationService;
 	
-	//TODO complete and add necessary methods
+	@GetMapping
+	public ResponseEntity<List<Application>> getAllApplications(){
+		return ResponseEntity.ok(applicationService.getAllApplications());
+	}
 	
-//	@PostMapping("/applicant")
-//	public ResponseEntity<Application> addApplication(@RequestBody @Valid Application application){
-//		
-//	}
-//	
-//	@GetMapping("{applicationId}")
-//	public ResponseEntity<Application> getApplicationById(int applicationId){}
+	@PostMapping("/applicant")
+	public ResponseEntity<Application> addApplication(@RequestBody @Valid Application application){
+		Application newApplication = applicationService.addApplication(application);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{applicationid}")
+				.buildAndExpand(newApplication.getApplicationId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@GetMapping("/applicant/{applicationid}")
+	public ResponseEntity<Application> getApplicationById(@PathVariable("applicationid") int applicationId){
+		Application application = applicationService.getApplicationById(applicationId);
+		if (application == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(application);
+		}
+	}
 }
--- application-service/src/main/java/com/cognixia/service/ApplicationService.java
+++ application-service/src/main/java/com/cognixia/service/ApplicationService.java
@@ -1,8 +1,12 @@
 package com.cognixia.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cognixia.common.exception.ApplicationNotFoundException;
 import com.cognixia.model.Application;
 import com.cognixia.repository.ApplicationRepository;
 
@@ -11,15 +15,21 @@ public class ApplicationService {
 	@Autowired
 	private ApplicationRepository applicationRepository;
 	
-	//TODO complete and add necessary methods
+	// retrieve full list of applications
+	public List<Application> getAllApplications(){
+		return applicationRepository.findAll();
+	}
 	
-//	public Application addApplication() {
-//		
-//	}
+	// add one application to database
+	public Application addApplication(Application application) {
+		application.setSubmissionDateTime(LocalDateTime.now());
+		application.setApplicationStatus("Submitted");
+		return applicationRepository.save(application);
+	}
 	
-	
-//	public Application getApplicationById() {
-//		
-//	}
+	// retrieve one application by searching for application id
+	public Application getApplicationById(int applicationId) {
+		return applicationRepository.findById(applicationId).orElseThrow(ApplicationNotFoundException::new);
+	}
 
 }
