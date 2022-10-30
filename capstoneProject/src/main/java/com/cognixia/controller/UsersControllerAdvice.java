4d1607c7b9de18bd2e7d5726017eae5730c9e4b6
SamanthaChia <43061854+SamanthaChia@users.noreply.github.com>
2022-10-30 21:04:37 +0800

    Revert "pull from main"

--- capstoneProject/src/main/java/com/cognixia/controller/UsersControllerAdvice.java
+++ capstoneProject/src/main/java/com/cognixia/controller/UsersControllerAdvice.java
@@ -5,15 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.cognixia.exception.PersonNotFoundException;
-
+import com.cognixia.exception.UsersNotFoundException;
 import com.cognixia.model.ErrorResponse;
 
 @RestControllerAdvice
-public class PersonControllerAdvice {
+public class UsersControllerAdvice {
 
-	@ExceptionHandler(PersonNotFoundException.class)
-	public ResponseEntity<ErrorResponse> usersNotFoundException (PersonNotFoundException e){
+	@ExceptionHandler(UsersNotFoundException.class)
+	public ResponseEntity<ErrorResponse> usersNotFoundException (UsersNotFoundException e){
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("UserService-404", "User(s) not found!" ));
 	}
 }
