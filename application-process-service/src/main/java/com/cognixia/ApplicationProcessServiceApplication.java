216bc4cd4fef8abfdfe4e7121abc6236780133a7
SamanthaChia <samantha1999@live.com.sg>
2022-11-02 20:14:43 +0800

    added new application process service

---
+++ application-process-service/src/main/java/com/cognixia/ApplicationProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationProcessServiceApplication.class, args);
+	}
+
+}
