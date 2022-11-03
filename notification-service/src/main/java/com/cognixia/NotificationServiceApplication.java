88c4d8a49db5b9d176aeffc60ec2b4c144192b3c
SamanthaChia <samantha1999@live.com.sg>
2022-11-03 13:03:06 +0800

    notification will take from application-process-service

--- notification-service/src/main/java/com/cognixia/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/cognixia/NotificationServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableScheduling
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
