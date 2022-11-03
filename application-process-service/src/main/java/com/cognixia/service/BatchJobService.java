2bd4826dd7eae03009571887dcdb3062ff598dc2
SamanthaChia <samantha1999@live.com.sg>
2022-11-03 11:10:28 +0800

    scheduler and automation working

--- application-process-service/src/main/java/com/cognixia/service/BatchJobService.java
+++ application-process-service/src/main/java/com/cognixia/service/BatchJobService.java
@@ -1,15 +1,19 @@
 package com.cognixia.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.batch.core.JobExecution;
-
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
@@ -22,23 +26,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.cognixia.SftpConfig.UploadGateway;
 
 @Service
 public class BatchJobService {
 	@Autowired
-	private UploadGateway gateway;
-
-	@Autowired
 	private JobLauncher jobLauncher;
 
-	@Qualifier("writeApplicationDataIntoJson")
+	@Qualifier("writeApplicationDataIntoSqlDb")
 	@Autowired
-	Job writeApplicationDataIntoJson;
+	Job writeApplicationDataIntoSqlDb;
 
 
 	
-	@Scheduled(cron = "0 59 10 * * ?")
+	@Scheduled(cron = "0 09 11 * * ?")
 	public void firstJobStarter() {
 		Map<String, JobParameter> params = new HashMap<>();
 
@@ -47,12 +47,11 @@ public class BatchJobService {
 
 		JobParameters jobParameters = new JobParameters(params);
 		try {
-
 			JobExecution jobExecution = 
-					jobLauncher.run(writeApplicationDataIntoJson, jobParameters);
+					jobLauncher.run(writeApplicationDataIntoSqlDb, jobParameters);
 			System.out.println("Job Execution Id = " +jobExecution.getJobId());
 			TimeUnit.SECONDS.sleep(5);
-			fileUpload();
+			
 		} catch (JobExecutionAlreadyRunningException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -73,11 +72,30 @@ public class BatchJobService {
 		
 	}
 
-	// upload file to sftp
-	public boolean fileUpload() {
+	//download json file from SFTP
+	@Scheduled(cron = "0 07 11 * * ?")
+	public static void downloadFile() throws IOException {
+		System.out.println("Downloading...");
 		Path currentRelativePath = Paths.get("");
 		String abs = currentRelativePath.toAbsolutePath().toString();
-		gateway.upload(new File(abs+"/src/main/resources/submissions.json"));
-		return true;
+		File source = new File("C:\\tmp\\ftp\\upload\\submissions.json");
+		File dest = new File(abs+"/src/main/resources/submissions.json");
+		
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			int length;
+			is = new FileInputStream(source);
+			os = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			
+			while((length = is.read(buffer)) > 0) {
+				os.write(buffer,0,length);
+			}
+		} finally {
+			is.close();
+			os.close();
+		}
+		
 	}
 }
